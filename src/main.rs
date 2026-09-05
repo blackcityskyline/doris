6c24625364bc6404c8ffe1f4983aefe0150393d5
koyaa <koyaanisqatsx8o3@gmail.com>
2026-09-05 19:27:19 +0300

    skeleton: main.rs with empty module declarations

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+mod cli;
+mod config;
+mod app;
+mod event;
+mod tui;
+mod browser;
+mod search;
+mod torrserver;
+mod bridge;
+mod ui;
+
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    Ok(())
+}
